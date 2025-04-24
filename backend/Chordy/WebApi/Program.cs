60b274671ebeba52493f8686e2d5c2a8e4d5b329
kirill_sadovnikov <89874515280@yandex.ru>
2025-04-24 22:58:21 +0400

    Initial frontend commit

--- backend/Chordy/WebApi/Program.cs
+++ backend/Chordy/WebApi/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000") // адрес фронта
+            policy.WithOrigins("http://localhost:5173") // адрес фронта
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
